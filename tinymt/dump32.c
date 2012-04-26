20eb8f54dac3279dd905ffc16bb855675825755c
Kenji Rikitake <kenji.rikitake@acm.org>
2012-04-26 14:48:20 +0900

    added dump32.c to do basic state checks

---
+++ tinymt/dump32.c
@@ -0,0 +1,58 @@
+/**
+ * @file dump32.c
+ *
+ * @brief Simple check program for tinymt32
+ *
+ * @author Kenji Rikitake (Kyoto University)
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (The University of Tokyo)
+ *
+ * Copyright (C) 2012 Kenji Rikitake and Kyoto University.
+ * All rights reserved.
+ *
+ * Copyright (C) 2011 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include "tinymt32.h"
+
+void state_dump(tinymt32_t *);
+
+void state_dump(tinymt32_t *r) {
+	printf("status[0] = %u, status[1] = %u, status[2] = %u, status[3] = %u\n",
+		r->status[0], r->status[1], r->status[2], r->status[3]);
+}	
+
+
+int main(int argc, char * argv[]) {
+    if (argc < 4) {
+	printf("%s mat1 mat2 tmat [seed]\n", argv[0]);
+	return -1;
+    }
+    tinymt32_t tinymt;
+    tinymt.mat1 = strtoul(argv[1], NULL, 16);
+    tinymt.mat2 = strtoul(argv[2], NULL, 16);
+    tinymt.tmat = strtoul(argv[3], NULL, 16);
+    int seed = 1;
+    if (argc >= 5) {
+	seed = strtol(argv[4], NULL, 0);
+    }
+    printf("tinymt32 0x%08"PRIx32, tinymt.mat1);
+    printf(" 0x%08"PRIx32, tinymt.mat2);
+    printf(" 0x%08"PRIx32, tinymt.tmat);
+    printf(" seed = %d\n", seed);
+    tinymt32_init(&tinymt, seed);
+    state_dump(&tinymt);
+    printf("32-bit unsigned integers r, where 0 <= r < 2^32\n");
+    for (int i = 0; i < 100; i++) {
+	    printf("%10"PRIu32"\n", tinymt32_generate_uint32(&tinymt));
+	    state_dump(&tinymt);
+    }
+}
